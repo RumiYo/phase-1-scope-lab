15f0cd6c435b426945943f615aff96483c70d151
RumiYo <yokoyamarumi@gmail.com>
2023-06-18 17:09:40 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName(){
+    customerName =  customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = "not bob";
+    return bestCustomer;
+}
+
+function overwriteBestCustomer(){
+    return bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "Karen";
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "Sandy";
+}
